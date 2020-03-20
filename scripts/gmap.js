f69442d732fb84010352e29b7aca0fbb15b6f126
Pedro-M-Santos <unbmatiaspedro@gmail.com>
2020-03-20 07:20:39 -0300

    Adding all files

---
+++ scripts/gmap.js
@@ -0,0 +1,16 @@
+// function initMap() 
+// {
+//   var myLatLng = {lat: -15.792246, lng: -47.881837};
+
+//   var map = new google.maps.Map(document.getElementById('gmap'), {
+//     zoom: 12,
+//     center: myLatLng
+//   });
+
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Hello World!'
+//   });
+
+// }
