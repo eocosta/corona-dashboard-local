e0bdb760933eddc076b9d226be1680fabe0e2dee
Pedro-M-Santos <unbmatiaspedro@gmail.com>
2020-03-22 12:45:58 -0300

    ramdom changes

---
+++ dist/test.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("p").append(" <b>Appended text</b>.");
+  });
+
+  $("#btn2").click(function(){
+    $("li").replaceWith("<li>Appended item</li>");
+  });
+});
